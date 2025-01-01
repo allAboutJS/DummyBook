20a0734f6a9b1173ad2adca4bbd5ef7c48a0cf0c
allAboutJS <victoronah1914@gmail.com>
2025-01-01 09:59:25 +0100

    Created `PostImageViewer` for viewing images in posts

---
+++ src/client/@types/PostImageViewer.ts
@@ -0,0 +1,6 @@
+interface PostImageViewerProps {
+    imagesToView: string[];
+    currentImageIndex: number;
+}
+
+export default PostImageViewerProps;
---
+++ src/client/components/PostImageViewer.tsx
@@ -0,0 +1,46 @@
+import { FaArrowLeft } from "react-icons/fa6";
+import { useState } from "react";
+import PostImageViewerProps from "../@types/PostImageViewer";
+
+function PostImageViewer(props: PostImageViewerProps) {
+    const [currentImageIndex, setCurrentImageIndex] = useState<number>(props.currentImageIndex);
+
+    return (
+        <div className="fixed inset-0 bg-zinc-100/50 dark:bg-black/50 backdrop-blur-md overflow-auto flex flex-col">
+            <header className="p-4 flex gap-4 items-center sticky top-0 dark:bg-zinc-800 bg-white">
+                <button
+                    onClick={() => window.history.back()}
+                    className="h-10 w-10 flex justify-center items-center dark:hover:bg-zinc-500 rounded-full hover:bg-zinc-300"
+                >
+                    <FaArrowLeft />
+                </button>
+                <h1 className="flex-1 text-center text-xl font-bold">Image Viewer</h1>
+            </header>
+            <div className="max-w-xl m-auto flex-1 flex items-center justify-center">
+                <img
+                    className="w-full h-auto"
+                    src={props.imagesToView[currentImageIndex]}
+                    alt={props.imagesToView[currentImageIndex]}
+                />
+            </div>
+            <footer className="sticky bottom-0 dark:bg-zinc-800 bg-white h-18 p-2 overflow-auto flex justify-center min-w-full">
+                <div className="flex gap-2 m-auto pr-4">
+                    {props.imagesToView.map((imgUrl, index) => (
+                        <img
+                            onClick={() => setCurrentImageIndex(index)}
+                            className={`h-16 w-auto ${
+                                currentImageIndex === index &&
+                                "dark:outline-zinc-100 outline-zinc-700 outline outline-offset-2"
+                            }`}
+                            src={imgUrl}
+                            alt={imgUrl}
+                            key={index}
+                        />
+                    ))}
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default PostImageViewer;
