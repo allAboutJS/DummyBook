311bbff5d8729499a6e4ebe04514b829eda41ff4
allAboutJS <victoronah1914@gmail.com>
2025-01-02 17:01:08 +0100

    Updated the `User` model with validation for `fullname` and `password` paths; Added some more paths: `gender`, `avatarImageUrl`, and `coverImageUrl`

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -1,24 +1,42 @@
-import { Document } from "mongoose";
 import mongoose, { connection } from "../config/db.config.js";
-
-export interface UserDocument extends Document {
-    fullname: string;
-    email: string;
-    password: string;
-}
+import UserDocument from "../@types/User.js";
 
 const userSchema = new mongoose.Schema<UserDocument>({
     fullname: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
+        index: true,
+        validate: {
+            validator: (value: string) => /^[\w'\-]+\s*([\w'\-]+)*$/.test(value)
+        }
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        trim: true,
+        index: true,
+        validate: {
+            validator: (value: string) => /^([a-zA-Z0-9.\-]{3,}@[a-zA-Z0-9]{2,}).[a-zA-Z0-9]{2,}$/.test(value)
+        }
     },
     password: {
         type: String,
         required: true
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female"],
+        required: true
+    },
+    avatarImageUrl: {
+        type: String,
+        default: "/images/default-user.png"
+    },
+    coverImageUrl: {
+        type: String,
+        default: "/images/default-cover-image.jpg"
     }
 });
 
