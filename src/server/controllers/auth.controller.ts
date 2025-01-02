a0dba90eb84f9efdf3b0d793a8b98c86daa29dd5
allAboutJS <victoronah1914@gmail.com>
2025-01-02 16:57:59 +0100

    Refactored registration and login logic

--- src/server/controllers/auth.controller.ts
+++ src/server/controllers/auth.controller.ts
@@ -4,63 +4,36 @@ import hashText from "../utils/hashText.js";
 import { encrypt } from "../utils/encryptDecrypt.js";
 
 export const handleSignUp: RequestHandler = async (req, res) => {
+    if (!req.is("application/json"))
+        return res.status(403).json({ status: "failed", message: "Invalid request body." });
+
     try {
-        const isJSONBody = req.is("application/json");
-
-        if (!isJSONBody) return res.status(403).json({ status: "failed", message: "Invalid request body." });
-
-        const { email, fullname, password } = req.body;
-
-        if (
-            !email ||
-            typeof email !== "string" ||
-            !password ||
-            typeof password !== "string" ||
-            !fullname ||
-            typeof fullname !== "string"
-        )
-            return res.status(400).json({ status: "failed", message: "Missing or invalid fields in request body." });
-
-        const userExists = await User.findOne({ email });
-
-        if (userExists) return res.status(409).json({ status: "failed", message: "Email already in use." });
-
-        await User.create({ email, fullname, password: hashText(password) });
+        await User.create({ ...req.body, password: hashText(req.body.password) });
         res.status(201).json({ status: "success", message: "User created successfully." });
-    } catch (error: any) {
-        console.error("Failed to register new user", error);
+    } catch {
         res.status(500).json({ status: "failed", message: "An unexpected error occurred." });
     }
 };
 
 export const handleLogin: RequestHandler = async (req, res) => {
+    if (!req.is("application/json"))
+        return res.status(403).json({ status: "failed", message: "Invalid request body." });
+
     try {
-        const isJSONBody = req.is("application/json");
-
-        if (!isJSONBody) return res.status(403).json({ status: "failed", message: "Invalid request body." });
-
         const { email, password } = req.body;
-
-        if (!email || typeof email !== "string" || !password || typeof password !== "string")
-            return res.status(400).json({ status: "failed", message: "Missing or invalid fields in request body." });
-
         const user = await User.findOne({ email });
+        let authToken: string, tokenExpiryDate: Date;
 
         if (!user) return res.status(404).json({ status: "failed", message: "No account was found for this email." });
 
         if (!hashText.verify(password, user.password))
             return res.status(401).json({ status: "failed", message: "Incorrect password." });
 
-        const authToken = encrypt(JSON.stringify({ email, expires: new Date(Date.now() + 3_600_000) }));
-
-        res.cookie("auth", authToken, {
-            httpOnly: true,
-            secure: true,
-            expires: new Date(Date.now() + 3_600_000) // 1 hour
-        });
+        authToken = encrypt(JSON.stringify({ email, expires: new Date(Date.now() + 3_600_000) }));
+        tokenExpiryDate = new Date(Date.now() + 3_600_000); // 1 hour
+        res.cookie("auth", authToken, { httpOnly: true, secure: true, expires: tokenExpiryDate });
         res.json({ status: "success", message: "Signed in successfully" });
-    } catch (error: any) {
-        console.error("Failed to register new user", error);
+    } catch {
         res.status(500).json({ status: "failed", message: "An unexpected error occurred." });
     }
 };
