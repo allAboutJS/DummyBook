f2a64c9c1b6450081c5a1ea1b85f39af5bcea473
allAboutJS <victoronah1914@gmail.com>
2025-01-02 17:05:50 +0100

    Added router for handling authenticated user API calls

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { handleLogin, handleSignUp } from "./controllers/auth.controller.js";
 import { connectToDb } from "./config/db.config.js";
+import usersApiRouter from "./routers/usersApiRouter.js";
 
 const app = express();
 const port = parseInt(process.env.PORT || "3000");
@@ -16,8 +17,9 @@ app.use(express.static(resolve(process.cwd(), "./public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api/user", usersApiRouter);
 
 app.post("/api/register", handleSignUp);
 app.post("/api/login", handleLogin);
 
-ViteExpress.listen(app, port, async () => (await connectToDb(), console.log("Server is listening on port 3000...")));
+ViteExpress.listen(app, port, async () => (await connectToDb(), console.log("Server is active")));
