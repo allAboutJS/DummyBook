1660005dc6d3b5d02fa8d4be91607ecf52aedd4b
allAboutJS <victoronah1914@gmail.com>
2025-01-02 17:16:13 +0100

    Created the database model for user posts

---
+++ src/server/models/Post.ts
@@ -0,0 +1,67 @@
+import PostDocument from "../@types/Post.js";
+import mongoose, { connection } from "../config/db.config.js";
+
+const postSchema = new mongoose.Schema<PostDocument>({
+    author: {
+        type: {
+            id: String,
+            fullname: String,
+            avatarImageUrl: String
+        },
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        default: Date.now
+    },
+    type: {
+        type: String,
+        enum: ["text_only", "text_and_image"],
+        required: true
+    },
+    impressions: {
+        likes: {
+            type: Number,
+            default: 0
+        },
+        dislikes: {
+            type: Number,
+            default: 0
+        },
+        comments: {
+            type: Number,
+            default: 0
+        },
+        shares: {
+            type: Number,
+            default: 0
+        }
+    },
+    styling: {
+        background: String,
+        foreground: String
+    },
+    data: {
+        type: {
+            text: {
+                type: String,
+                required: function () {
+                    return this.type === "text_only";
+                }
+            },
+            images: {
+                type: [String],
+                required: function () {
+                    return this.type === "text_and_image";
+                },
+                validate: {
+                    validator: (value: any) => (Array.isArray(value) ? Boolean(value.length) : false)
+                }
+            }
+        }
+    }
+});
+
+const Post = connection.model("Post", postSchema);
+
+export default Post;
