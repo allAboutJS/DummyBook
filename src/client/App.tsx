2cc41be2eef5b83764430ceafb1585639179bd44
allAboutJS <victoronah1914@gmail.com>
2025-01-03 09:05:59 +0100

    Removed redundant code

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import { useAppSelector, useAppDispatch } from "./store/hooks";
-import { login, logout } from "./store/slices/authSlice";
 import router from "./routes/router";
 
 function App() {
-    const auth = useAppSelector((state) => state.auth);
-    const dispatch = useAppDispatch();
-
     return <RouterProvider router={router} />;
 }
 
